2150672a97c4a815469a9a070e8b59442871df92
Dhruvil Shah <dhruvilkoup0704@gmail.com>
2024-05-01 17:18:38 +0530

    Added basic spring modules including securty

---
+++ src/main/java/com/bits/wilp/fsa/langLearnModel/LangLearnModelApplication.java
@@ -0,0 +1,13 @@
+package com.bits.wilp.fsa.langLearnModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangLearnModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangLearnModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bits/wilp/fsa/langLearnModel/LangLearnModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.wilp.fsa.langLearnModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangLearnModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
