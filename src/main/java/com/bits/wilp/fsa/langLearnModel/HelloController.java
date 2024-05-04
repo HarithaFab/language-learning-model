80d03ae10e8841cedd771161d7eeb0a748870db8
Dhruvil Shah <dhruvilkoup0704@gmail.com>
2024-05-04 15:25:08 +0530

    serving image

--- src/main/java/com/bits/wilp/fsa/langLearnModel/HelloController.java
+++ src/main/java/com/bits/wilp/fsa/langLearnModel/HelloController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.HashMap;
 import com.google.gson.Gson;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HelloController {
@@ -15,6 +20,18 @@ public class HelloController {
         translator.put("how","cómo");
         translator.put("morning","mañana");
         return translator.get(word);
+    }
+
+    @GetMapping("/image/{filename}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("filename") String filename) {
+        byte[] image = new byte[0];
+        String context = "E://personal/BITS-WILP/Sem2/Full Stack/assignment/language-learning-model/src/main/java/com/bits/wilp/fsa/langLearnModel/";
+        try {
+            image = FileUtils.readFileToByteArray(new File(context+filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
     }
 
     @GetMapping("/spanish/readingLesson/{id}")
