e08982b6095b8ea7d07ff63bc480c34f81cc7765
Dhruvil Shah <dhruvilkoup0704@gmail.com>
2024-05-04 12:02:28 +0530

    added translate and reading lesson content

---
+++ src/main/java/com/bits/wilp/fsa/langLearnModel/HelloController.java
@@ -0,0 +1,44 @@
+package com.bits.wilp.fsa.langLearnModel;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.HashMap;
+import com.google.gson.Gson;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/translate/spanish/{word}")
+    public String translateHello(@PathVariable String word) {
+        HashMap<String,String> translator = new HashMap<String,String>();
+        translator.put("hello","Hola");
+        translator.put("how","cómo");
+        translator.put("morning","mañana");
+        return translator.get(word);
+    }
+
+    @GetMapping("/spanish/readingLesson/{id}")
+    public String readingLesson(@PathVariable int id) {
+        HashMap<Integer, HashMap<String,String>> lessons = new HashMap<>();
+        HashMap<String,String> translator = new HashMap<>();
+        Gson lessonDetails = new Gson();
+        translator.put("hello","Hola");
+        translator.put("how","cómo");
+        translator.put("morning","mañana");
+        lessons.put(1,translator);
+        
+        translator = new HashMap<>();
+        translator.put("apple","manzana");
+        translator.put("orange","naranja");
+        translator.put("muskmelon","melón");
+        lessons.put(2,translator);
+
+        translator = new HashMap<>();
+        translator.put("red","roja");
+        translator.put("yellow","amarilla");
+        translator.put("green","verde");
+        lessons.put(3,translator);
+        String json = lessonDetails.toJson(lessons.get(id));
+        return json;
+    }
+}
